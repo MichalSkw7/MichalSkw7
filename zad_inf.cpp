48b5b4946defbe0d138ec8f3987ab867bd44d184
MichalSkw7 <michau.skwierz@gmail.com>
2024-10-29 21:58:49 +0100

    Update zad_inf.cpp

--- zad_inf.cpp
+++ zad_inf.cpp
@@ -1,32 +1,32 @@
 ﻿#include <iostream>`
 using namespace std;
   
-  int wyszukiwanie_binarne(int tablica[], int lewy, int prawy, int szukana) {
+  int szukajka(int tablica[], int lewy, int prawy, int szukana)
+{
       while (lewy <= prawy) 
       {
           int srodek = lewy + (prawy - lewy) / 2;
-          // Sprawdzenie, czy szukany element jest w środku
+          // Sprawdzenie, czy element jest w środku
           if (tablica[srodek] == szukana)
-              return srodek; // Zwraca indeks elementu
-          // Jeśli szukana liczba jest większa, przeszukujemy prawą połowę
+              return srodek; // Zwraca indeks szukanego elementu
           if (tablica[srodek] < szukana)
               lewy = srodek + 1;
-          // Jeśli szukana liczba jest mniejsza, przeszukujemy lewą połowę
+          // Jeśli szukana liczba jest mniejsza przeszukuje lewą połowę zbioru
           else
               prawy = srodek - 1;
       }
       return -1;
-  }
+}
 int main() 
 {
     int tablica[15] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };  // Posortowana tablica o rozmiarze 15
     int szukana;
-    cout << "Podaj liczbe do wyszukania: ";
+      cout << "Podaj liczbe do wyszukania: ";
     cin >> szukana;
-    int wynik = wyszukiwanie_binarne(tablica, 0, 14, szukana);  // Rozmiar tablicy to 15
+    int wynik = szukajka(tablica, 0, 14, szukana); 
     if (wynik != -1)
-        cout << "Element znaleziony na indeksie: " << wynik << endl;
+        cout << "Element znaleziony w indeksie: " << wynik << endl;
     else
-        cout << "Element nie znaleziony" << endl;
+        cout << "Element nie został znaleziony" << endl;
     return 0;
 }
